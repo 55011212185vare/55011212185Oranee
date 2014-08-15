5b4051645f97a97b3ac32bcaf2adafe34d0d9f30
55011212185vare <imac>
2014-08-15 17:05:52 +0700

    งาน

---
+++ switftTurial1.playground/section-1.swift
@@ -0,0 +1,27 @@
+let tutorialTeam = 56
+let editorialTeam = 23
+let totalTeam = tutorialTeam + editorialTeam
+  totalTeam += 1
+let priceIntInferred = -19
+let priceIntExplicit = -19
+
+let priceFloatferred = -19.99
+let priceFloatExplicit  = -19.99
+
+let onsaleInferred = true
+let onsaleExplicit:Bool = true
+
+let nameInferred = "whoopie cushion"
+let nameExplicit:String="whoopie cushion"
+
+
+
+
+
+
+
+
+
+
+
+    
